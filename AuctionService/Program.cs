1fc48a8e5e560715f4f54b6fd01924a58e7a7a6e
brnooliveira <breno.osales123@gmail.com>
2025-01-02 23:05:54 -0300

    escondendo as variaveis

--- AuctionService/Program.cs
+++ AuctionService/Program.cs
@@ -2,16 +2,26 @@ using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.Load("../.env");
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+
+    // Adiciona variáveis de ambiente por último (sobrescrevem o que veio antes)
+    .AddEnvironmentVariables();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 builder.Services.AddDbContext<AuctionDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
