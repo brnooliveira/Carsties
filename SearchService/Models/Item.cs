8af73ad6a49a64c13583d3bd0301810f55ca5b1f
brnooliveira <breno.osales123@gmail.com>
2025-01-06 18:28:23 -0300

    Add mongo db

---
+++ SearchService/Models/Item.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MongoDB.Entities;
+
+namespace SearchService.Models;
+
+public class Item : Entity
+{
+    [Column("reserved_price")] public int ReservedPrice { get; set; }
+
+    [Column("seller")] [MaxLength(50)] public string Seller { get; set; }
+
+    [Column("winner")] [MaxLength(50)] public string Winner { get; set; }
+
+    [Column("sold_amount")] public int SoldAmount { get; set; }
+
+    [Column("current_high_bid")] public int CurrentHighBid { get; set; }
+
+    [Column("created_at")] public DateTime CreatedAt { get; set; }
+
+    [Column("updated_at")] public DateTime UpdateAt { get; set; }
+
+    [Column("auction_end")] public DateTime AuctionEnd { get; set; }
+
+    [Column("status")] public string Status { get; set; }
+
+    [Column("make")] [StringLength(50)] public string Make { get; set; }
+
+    [Column("model")] [StringLength(50)] public string Model { get; set; }
+
+    [Column("year")] public int Year { get; set; }
+
+    [Column("color")] [StringLength(30)] public string Color { get; set; }
+
+    [Column("mileage")] public int Mileage { get; set; }
+
+    [Column("image_url")]
+    [StringLength(200)]
+    public string ImageUrl { get; set; }
+}
--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,4 +1,16 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.Load("../.env");
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+
+    // Adiciona variáveis de ambiente por último (sobrescrevem o que veio antes)
+    .AddEnvironmentVariables();
 
 // Add services to the container.
 
@@ -12,5 +24,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
